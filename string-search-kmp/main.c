3ae0e6e96e69deae0e89ca98cda5817c2d335659
hit9 <hit9@icloud.com>
2021-02-25 00:20:18 +0800

    Minor fix comments

--- string-search-kmp/main.c
+++ string-search-kmp/main.c
@@ -3,13 +3,13 @@
 
 #include <stdio.h>  // for printf
 
-// ComputeNext 为给定的模式串 p 计算 next 数组.
+// ComputeNext 为长度为 m 的模式串 p 计算 next 数组.
 // next 数组的含义：
 // 取字符串 p 的位置 j 之前的前缀字符串 p' ，其头尾的最大公共长度即 next[j]
 // 此处采用递推方法实现
-void ComputeNext(char *p, int p_length, int next[]) {
+void ComputeNext(char *p, int m, int next[]) {
     next[0] = 0;
-    if (p_length <= 1) return;
+    if (m <= 1) return;
 
     // p' 是单个字符时，认为它无公共头尾
     next[1] = 0;
@@ -18,11 +18,8 @@ void ComputeNext(char *p, int p_length, int next[]) {
     // p' 是字符串 p 的长度为 j 的子串
     int j = 2;
 
-    while (j < p_length) {
-        int last = j - 1;
-        int left = next[j - 1];
-
-        if (p[last] == p[left]) {
+    while (j < m) {
+        if (p[j - 1] == p[next[j - 1]]) {
             next[j] = next[j - 1] + 1;
         } else {
             next[j] = 0;
@@ -32,25 +29,25 @@ void ComputeNext(char *p, int p_length, int next[]) {
     }
 }
 
-// KMP 从字符串 s 中查找字符串 p ，返回其位置.
+// KMP 从长度为 n 的字符串 s 中查找长度为 m 的字符串 p ，返回其位置.
 // 找不到则返回 -1
-int KMP(char *s, int s_length, char *p, int p_length) {
+int KMP(char *s, int n, char *p, int m) {
     // 预处理 next 数组
-    int next[p_length];
-    ComputeNext(p, p_length, next);
+    int next[m];
+    ComputeNext(p, m, next);
 
     // i 遍历 s
     // j 遍历 p
     int i = 0;
     int j = 0;
 
-    while (i < s_length) {
+    while (i < n) {
         // 匹配
         if (s[i] == p[j]) {
-            if (j == p_length - 1) {
+            if (j == m - 1) {
                 // 子串匹配到尾部，说明命中
                 // 返回匹配的起始位置
-                return i + 1 - p_length;
+                return i + 1 - m;
             } else {
                 // 否则，则继续匹配
                 i++;
