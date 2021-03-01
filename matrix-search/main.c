dcb7b7c9e4bc9fee5c126576554cf3bf8ee59e40
hit9 <hit9@icloud.com>
2021-03-01 15:27:40 +0800

    Add matrix search

---
+++ matrix-search/main.c
@@ -0,0 +1,44 @@
+// 从二维矩阵中查找一个数字
+
+#include <assert.h>
+
+// Result 是查找的结果
+struct Result {
+    int row;
+    int col;
+};
+
+// MatrixSearch 从给定的行数为 row ，列数为 col 的矩阵 matrix 中查找 target
+// 如果存在，则返回 Result ，否则，返回 Result{-1,-1} 。
+// 其中，matrix 的行和列都是严格递增的
+// 比如 1 4 7
+//      2 5 8
+//      3 6 9
+struct Result MatrixSearch(int target, int rows, int cols,
+                           int matrix[rows][cols]) {
+    // 从右上角开始探索
+    int row = 0;
+    int col = cols - 1;
+
+    while (row < rows && col > 0) {
+        if (matrix[row][col] < target) {
+            row++;
+        } else if (matrix[row][col] > target) {
+            col--;
+        } else {
+            return (struct Result){row, col};
+        }
+    }
+    return (struct Result){-1, -1};
+}
+
+int main(void) {
+    int matrix[3][4] = {
+        {1, 4, 7, 10},
+        {2, 5, 8, 11},
+        {3, 6, 9, 12},
+    };
+
+    struct Result result = MatrixSearch(9, 3, 4, matrix);
+    assert(result.row == 2 && result.col == 2);
+}
