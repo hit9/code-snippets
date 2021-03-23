6a8d90d61c1dce886cf6228f1005248becd76a96
hit9 <hit9@icloud.com>
2021-03-23 23:10:26 +0800

    Update

--- data-structure-heap/main.c
+++ data-structure-heap/main.c
@@ -23,7 +23,6 @@ void Build(int a[], int n);        // 堆化
 int Push(int a[], int n, int v);   // 插入
 int Pop(int a[], int n);           // 删除
 int Top(int a[]);                  // 堆顶
-void Fix(int a[], int i);          // 修整
 
 ///////////
 // 实现
@@ -100,20 +99,23 @@ void Swap(int a[], int i, int j) {
 }
 
 ///////////
-// 测试
+// Example
 ///////////
 
 int main(void) {
     int n = 6;
     int a[32] = {3, 1, 2, 7, 0, 4};
 
-    // Build heap
+    // 堆化
     Build(a, n);
 
-    // Push
+    // 插入
     n = Push(a, n, 6);
 
-    // Pop and print
+    // 堆顶
+    int min = Top(a);
+
+    // 不断弹出并打印
     while (n > 0) {
         int v = Pop(a, n--);
         printf("%d ", v);
