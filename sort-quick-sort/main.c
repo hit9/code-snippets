66be790d264f6e665a6f4606a2be0c17faa76fb5
hit9 <hit9@icloud.com>
2021-03-25 13:48:51 +0800

    Update quick-sort

--- sort-quick-sort/main.c
+++ sort-quick-sort/main.c
@@ -10,7 +10,7 @@ void Swap(int a[], int i, int j) {
 
 // 分割函数，选取第一个为基准元素
 // 把 start 到 end 内的元素，小于基准元素的放在左边，大于的放在右边
-// 返回基准值的右边界位置，也是 <= 基准值的个数
+// 返回基准值的最右下标
 int Partition(int a[], int start, int end) {
     int k = a[start];
     int left = start;
@@ -29,7 +29,7 @@ int Partition(int a[], int start, int end) {
             i++;
         }
     }
-    return i;
+    return i - 1;
 }
 
 // 快速排序 - 递归实现
@@ -37,12 +37,12 @@ void QuickSort(int a[], int start, int end) {
     if (start >= end) return;
     int p = Partition(a, start, end);
     QuickSort(a, start, p - 1);
-    QuickSort(a, p, end);
+    QuickSort(a, p + 1, end);
 }
 
 int main(void) {
     int n = 9;
-    int a[] = {4, 1, 3, 9, 7, 2, 6, 8, 5};
+    int a[] = {5, 1, 3, 9, 7, 2, 5, 8, 4};
     QuickSort(a, 0, n - 1);
     for (int i = 0; i < n; i++) printf("%d ", a[i]);
 }
