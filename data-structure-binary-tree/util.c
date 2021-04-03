a7ef532a6871332e10575fb50dc20152846da385
hit9 <hit9@icloud.com>
2021-04-03 17:18:21 +0800

    Update util

---
+++ data-structure-binary-tree/util.c
@@ -0,0 +1,207 @@
+#include "util.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/////////////////
+// 整型数组 (动态内存)
+/////////////////
+
+IntArray *NewIntArray() {
+    IntArray *a = malloc(sizeof(IntArray));
+    a->a = malloc(32 * sizeof(int));
+    a->cap = 32;
+    a->n = 0;
+    return a;
+}
+
+void FreeIntArray(IntArray *a) {
+    if (a != NULL) {
+        if (a->a != NULL) free(a->a);
+        free(a);
+    }
+}
+
+bool IntArrayPush(IntArray *a, int v) {
+    if (a->cap < a->n + 1) {
+        int cap = a->cap + 32;
+        int *a1 = realloc(a->a, cap * sizeof(int));
+        if (a1 == NULL) return false;
+        a->a = a1;
+        a->cap = cap;
+    }
+    a->a[a->n++] = v;
+    return true;
+}
+
+int IntArraySize(IntArray *a) {
+    if (a == NULL) return 0;
+    return a->n;
+}
+
+int IntArrayPop(IntArray *a) {
+    if (a == NULL) return -1;
+    return a->a[--a->n];
+}
+
+int *IntArrayUnwrap(IntArray *a) {
+    if (a == NULL) return NULL;
+    return a->a;
+}
+
+/////////////////
+// 动态数组 (一般类型)
+/////////////////
+
+Array *NewArray() {
+    Array *a = malloc(sizeof(Array));
+    a->a = malloc(32 * sizeof(void *));
+    a->cap = 32;
+    a->n = 0;
+    return a;
+}
+
+void FreeArray(Array *a) {
+    if (a != NULL) {
+        if (a->a != NULL) free(a->a);
+        free(a);
+    }
+}
+
+bool ArrayPush(Array *a, void *v) {
+    if (a->cap < a->n + 1) {
+        int cap = a->cap + 32;
+        void **a1 = realloc(a->a, cap * sizeof(void *));
+        if (a1 == NULL) return false;
+        a->a = a1;
+        a->cap = cap;
+    }
+    a->a[a->n++] = v;
+    return true;
+}
+
+int ArraySize(Array *a) {
+    if (a == NULL) return 0;
+    return a->n;
+}
+
+void *ArrayPop(Array *a) {
+    if (a == NULL) return -1;
+    return a->a[--a->n];
+}
+
+void **ArrayUnwrap(Array *a) {
+    if (a == NULL) return NULL;
+    return a->a;
+}
+
+/////////////////
+// 栈 (动态内存)
+/////////////////
+
+Stack *NewStack() {
+    Stack *stack = malloc(sizeof(Stack));
+    stack->a = NewArray();
+    return stack;
+}
+
+void FreeStack(Stack *stack) {
+    if (stack != NULL) {
+        FreeArray(stack->a);
+        free(stack);
+    }
+}
+
+bool StackPush(Stack *stack, void *v) { return ArrayPush(stack->a, v); }
+
+void *StackPop(Stack *stack) { return ArrayPop(stack->a); }
+
+bool IsStackEmpty(Stack *stack) {
+    if (stack == NULL || ArraySize(stack->a) == 0) return true;
+    return false;
+}
+
+void *StackTop(Stack *stack) {
+    if (IsStackEmpty(stack)) return NULL;
+    void **a = ArrayUnwrap(stack->a);
+    int n = ArraySize(stack->a);
+    return a[n - 1];
+}
+
+/////////////////
+// 队列 (动态内存)
+/////////////////
+
+// 创建一个队列节点
+QNode *NewQNode(void *v) {
+    QNode *node = malloc(sizeof(QNode));
+    node->v = v;
+    node->next = NULL;
+    return node;
+}
+
+// 释放一个队列节点
+void FreeQNode(QNode *node) { free(node); }
+
+// 创建一个队列
+Queue *NewQueue() {
+    Queue *q = malloc(sizeof(Queue));
+    q->head = NULL;
+    q->tail = NULL;
+    q->length = 0;
+    return q;
+}
+
+// 是否队空？
+bool IsQueueEmpty(Queue *q) {
+    if (q == NULL || q->length == 0) return true;
+    return false;
+}
+
+// 获取队列头, 空返回 NULL
+void *QueueFront(Queue *q) {
+    if (IsQueueEmpty(q)) return NULL;
+    return q->head->v;
+}
+
+// 数据入队
+void QueuePush(Queue *q, void *v) {
+    if (q == NULL) return;
+
+    QNode *node = NewQNode(v);
+
+    if (IsQueueEmpty(q)) {
+        q->head = node;
+        q->tail = node;
+    } else {
+        q->tail->next = node;
+        q->tail = node;
+    }
+    q->length++;
+}
+
+// 队头出队, 空返回 NULL
+void *QueuePop(Queue *q) {
+    if (IsQueueEmpty(q)) return NULL;
+
+    QNode *head = q->head;
+
+    if (q->tail == head) q->tail = NULL;
+    q->head = head->next;
+    q->length--;
+
+    void *v = head->v;
+    FreeQNode(head);
+    return v;
+}
+
+// 释放一个队列
+void FreeQueue(Queue *q) {
+    while (!IsQueueEmpty(q)) QueuePop(q);
+    free(q);
+}
+// 获取队列大小
+int QueueSize(Queue *q) {
+    if (q == NULL) return 0;
+    return q->length;
+}
---
+++ data-structure-binary-tree/util.h
@@ -0,0 +1,66 @@
+// 辅助函数和数据结构
+
+#include <stdbool.h>
+
+// 动态数组（整型）
+typedef struct _IntArray {
+    int *a;
+    int n;
+    int cap;
+} IntArray;
+
+IntArray *NewIntArray();
+void FreeIntArray(IntArray *a);
+bool IntArrayPush(IntArray *a, int v);
+int IntArraySize(IntArray *a);
+int IntArrayPop(IntArray *a);
+int *IntArrayUnwrap(IntArray *a);
+
+// 动态数组（一般类型）
+typedef struct _Array {
+    void **a;
+    int n;
+    int cap;
+} Array;
+
+Array *NewArray();
+void FreeArray(Array *a);
+bool ArrayPush(Array *a, void *v);
+int ArraySize(Array *a);
+void *ArrayPop(Array *a);
+void **ArrayUnwrap(Array *a);
+
+// 栈
+typedef struct _Stack {
+    Array *a;
+} Stack;
+
+Stack *NewStack();
+void FreeStack(Stack *stack);
+bool StackPush(Stack *stack, void *v);
+void *StackPop(Stack *stack);
+bool IsStackEmpty(Stack *stack);
+void *StackTop(Stack *stack);
+
+// 单端队列
+typedef struct _QNode {
+    void *v;
+    struct _QNode *next;
+} QNode;
+
+typedef struct _Queue {
+    int length;
+    QNode *head;
+    QNode *tail;
+} Queue;
+
+QNode *NewQNode(void *v);     // 创建队列节点
+void FreeQNode(QNode *node);  // 释放队列节点
+
+Queue *NewQueue();                  // 创建队列
+void FreeQueue(Queue *q);           // 释放队列
+bool IsQueueEmpty(Queue *q);        // 是否队空？
+void *QueueFront(Queue *q);         // 获取队头
+void QueuePush(Queue *q, void *v);  // 数据入队
+void *QueuePop(Queue *q);           // 队头出队
+int QueueSize(Queue *q);            // 队列大小
