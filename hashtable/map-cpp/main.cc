9eb280b7b6af97625de6fb8437ee0b331ab1f577
hit9 <hit9@icloud.com>
2023-07-24 22:18:18 +0800

    update map-cpp

--- hashtable/map-cpp/main.cc
+++ hashtable/map-cpp/main.cc
@@ -3,16 +3,29 @@
 #include "map.hpp"
 
 int main(void) {
-    Map<int, int, 5> m;
+    Map<int, int> m;
     std::cout << m.Cap() << std::endl;
-    std::cout << std::boolalpha << m.Set(5, 10) << std::endl;  // true
-    std::cout << std::boolalpha << m.Set(6, 12) << std::endl;  // true
-    std::cout << std::boolalpha << m.Set(7, 13) << std::endl;  // true
-    std::cout << m.Get(5) << std::endl;                        // 10
-    std::cout << m.Get(6) << std::endl;                        // 12
-    std::cout << m.Get(7) << std::endl;                        // 13
-    std::cout << m.Size() << std::endl;                        // 3
-    std::cout << m.Pop(7) << std::endl;                        // 13
-    std::cout << m.Size() << std::endl;                        // 2
+    std::cout << std::boolalpha << m.Set(5, 10) << std::endl;   // true
+    std::cout << std::boolalpha << m.Set(6, 12) << std::endl;   // true
+    std::cout << std::boolalpha << m.Set(7, 13) << std::endl;   // true
+    std::cout << m.Get(5) << std::endl;                         // 10
+    std::cout << m.Get(6) << std::endl;                         // 12
+    std::cout << m.Get(7) << std::endl;                         // 13
+    std::cout << m.Size() << std::endl;                         // 3
+    std::cout << m.Pop(7) << std::endl;                         // 13
+    std::cout << m.Size() << std::endl;                         // 2
+    std::cout << std::boolalpha << m.Set(8, 14) << std::endl;   // true
+    std::cout << std::boolalpha << m.Set(9, 15) << std::endl;   // true
+    std::cout << std::boolalpha << m.Set(10, 16) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(11, 17) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(12, 18) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(13, 19) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(14, 20) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(15, 21) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(16, 22) << std::endl;  // true
+    std::cout << std::boolalpha << m.Set(17, 23) << std::endl;  // true
+    std::cout << m.Cap() << std::endl;                          // 16
+    std::cout << m.Size() << std::endl;                         // 10
+
     return 0;
 }
--- hashtable/map-cpp/map.hpp
+++ hashtable/map-cpp/map.hpp
@@ -1,7 +1,6 @@
-// 一个简单的 Map 实现
-// 1. 无排序
-// 2. 采用哈希开放寻址
-// 3. 没实现扩容
+// 一个简单的堆上 Map 实现
+// 1. 采用哈希开放寻址
+// 2. 带自动扩容 (容量倍增)
 // 编译选项 -std=c++20
 
 #include <cstddef>
@@ -10,7 +9,8 @@
 // K 是 Key 的类型
 // V 是 Value 的类型
 // hash 是哈希函数
-template <typename K, typename V, std::size_t N, auto hash = std::hash<K>{}>
+// load_factor 是负载因子
+template <typename K, typename V, auto hash = std::hash<K>{}>
 class Map {
    private:
     // 节点
@@ -23,27 +23,63 @@ class Map {
     };
 
     // 最大容量
-    static const std::size_t cap = N * 1.2;
+    std::size_t cap = 8;
+
     // 底层 buffer
-    Node b[cap];
+    Node* b = nullptr;
+
     // 元素计数
     size_t n = 0;
 
+    // 负载因子
+    static constexpr double load_factor = 0.75;
+
    public:
-    Map() = default;
+    // 构造空 Map
+    Map() { b = new Node[cap]{}; };
 
     // 开放寻址法返回位置
-    std::size_t Hash(K k, size_t i) const { return (hash(k) + i) % cap; }
+    std::size_t Hash(K k, size_t i, size_t cap) const {
+        return (hash(k) + i) % cap;
+    }
 
     // 大小
     size_t Size() const { return n; };
     size_t Cap() const { return cap; };
 
+    // 扩展, 按两倍进行
+    void Resize() {
+        auto new_cap = 2 * cap;
+        auto new_b = new Node[new_cap]{};
+
+        // 拷贝、重新 hash
+        for (auto i = 0; i < cap; i++) {
+            auto& node = b[i];
+            if (node.used) {
+                for (auto j = 0; j < new_cap; j++) {
+                    auto p = Hash(node.k, j, new_cap);
+                    auto& new_node = new_b[p];
+                    if (!new_node.used) {
+                        new_node.k = node.k;
+                        new_node.v = node.v;
+                        new_node.used = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        cap = new_cap;
+        b = new_b;
+    }
+
     // 添加或更新键 k 的值为 v
     // 如果 m 不足以容纳，返回 false
     bool Set(K k, V v) {
+        if (cap * load_factor < n) Resize();
+
         for (auto i = 0; i < cap; i++) {
-            auto p = Hash(k, i);
+            auto p = Hash(k, i, cap);
             auto& node = b[p];
             // 添加或更新
             // 开放寻址: 冲突时即 node.used 且 node.k != k,
@@ -62,7 +98,7 @@ class Map {
     // 获取键 k 的值，如果不存在则报错
     V Get(K k) const {
         for (auto i = 0; i < cap; i++) {
-            auto p = Hash(k, i);
+            auto p = Hash(k, i, cap);
             auto& node = b[p];
             if (node.used && node.k == k) return node.v;
         }
@@ -72,7 +108,7 @@ class Map {
     // 测试键 k 是否在表 m 中
     bool Has(K k) const {
         for (auto i = 0; i < cap; i++) {
-            auto p = Hash(k, i);
+            auto p = Hash(k, i, cap);
             auto& node = b[p];
             if (node.used && node.k == k) return true;
         }
@@ -82,7 +118,7 @@ class Map {
     // 弹出键 k，返回它的值。如果不存在则报错
     V Pop(K k) {
         for (auto i = 0; i < cap; i++) {
-            auto p = Hash(k, i);
+            auto p = Hash(k, i, cap);
             auto& node = b[p];
             if (node.used && node.k == k) {
                 node.used = false;
