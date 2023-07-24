8d6f2d9e61fc9405ca94a8eedbfd62610c02c190
hit9 <hit9@icloud.com>
2023-07-24 17:47:38 +0800

    Split into hh

---
+++ skiplist/skiplist-cpp/main.cc
@@ -0,0 +1,25 @@
+// clang++ main.cc -std=c++20
+
+#include "skiplist.hh"
+
+int main(void) {
+    Skiplist<int> s;
+
+    s.Put(3);
+    s.Put(9);
+    s.Put(1);
+    s.Put(4);
+    s.Put(5);
+    s.Put(8);
+    s.Put(7);
+    s.Put(2);
+    s.Put(10);
+
+    s.Print();
+
+    std::cout << s.Has(3) << std::endl;  // 1
+    std::cout << s.Top() << std::endl;   // 1
+    s.PopFirst();
+    std::cout << s.Top() << std::endl;  // 2
+    return 0;
+}
--- skiplist/skiplist-cpp/skiplist.hh
+++ skiplist/skiplist-cpp/skiplist.hh
@@ -172,25 +172,3 @@ class Skiplist {
         }
     }
 };
-
-int main(void) {
-    Skiplist<int> s;
-
-    s.Put(3);
-    s.Put(9);
-    s.Put(1);
-    s.Put(4);
-    s.Put(5);
-    s.Put(8);
-    s.Put(7);
-    s.Put(2);
-    s.Put(10);
-
-    s.Print();
-
-    std::cout << s.Has(3) << std::endl;  // 1
-    std::cout << s.Top() << std::endl;   // 1
-    s.PopFirst();
-    std::cout << s.Top() << std::endl;  // 2
-    return 0;
-}
