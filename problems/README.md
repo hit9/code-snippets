8c6689666ebf04564342ee38af43ed555090e340
hit9 <hit9@icloud.com>
2023-11-05 14:09:02 +0800

    Add leetcode-1262

--- problems/README.md
+++ problems/README.md
@@ -74,3 +74,4 @@
 - [leetcode-35 搜索插入位置](leetcode-35-search-insert-position)  ⭐️ (二分)
 - [leetcode-1339 分裂二叉树的最大乘积](leetcode-1339-maximum-product-of-splitted-binary-tree)  (二叉树)
 - [leetcode-124 二叉树中的最大路径和](leetcode-124-binary-tree-maximum-path-sum)     (动态规划、树上 DP)
+- [leetcode-1262 可被三整除的最大和](leetcode-1262-greatest-sum-divisible-by-three)        ⭐️⭐️⭐️⭐️  (动态规划)
---
+++ problems/leetcode-1262-greatest-sum-divisible-by-three/main.cc
@@ -0,0 +1,49 @@
+// https://leetcode.cn/problems/greatest-sum-divisible-by-three
+// 1262. 可被三整除的最大和
+// 给你一个整数数组 nums，请你找出并返回能被三整除的元素最大和。
+//
+// 示例 1：
+// 输入：nums = [3,6,5,1,8]
+// 输出：18
+// 解释：选出数字 3, 6, 1 和 8，它们的和是 18（可被 3 整除的最大和）。
+//
+// 示例 2：
+//
+// 输入：nums = [4]
+// 输出：0
+// 解释：4 不能被 3 整除，所以无法选出数字，返回 0。
+//
+// 示例 3：
+//
+// 输入：nums = [1,2,3,4,4]
+// 输出：12
+// 解释：选出数字 1, 3, 4 以及 4，它们的和是 12（可被 3 整除的最大和）。
+//
+// 提示：
+// 1 <= nums[i] <= 10^4
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    int maxSumDivThree(vector<int>& nums) {
+        // 跟进余数为 0,1,2 的最大和的值
+        int R[3] = {0, 0, 0};
+
+        for (auto num : nums) {
+            // A, B, C 为考虑选择 num 的情况下的值
+            int A = R[0] + num;
+            int B = R[1] + num;
+            int C = R[2] + num;
+
+            // 更新对应槽位的值, 如果更大，才更新
+            R[A % 3] = std::max(A, R[A % 3]);
+            R[B % 3] = std::max(B, R[B % 3]);
+            R[C % 3] = std::max(C, R[C % 3]);
+        }
+
+        return R[0];
+    }
+};
