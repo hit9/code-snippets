ca0577e849a4c68cc9816cb54201409ccdc8022f
hit9 <hit9@icloud.com>
2023-11-16 12:32:16 +0800

    快速幂模板题

---
+++ problems/luogu/P1226/main.cc
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+int fastpowermod(int a, int b, int p) {
+    int factor = a;
+    int result = 1;
+    int remain = b;
+    while (remain > 0) {
+        if (remain & 1) {
+            // 奇数
+            result = (long long)result * factor % p;
+            remain--;
+        } else {
+            // 偶数
+            remain = remain / 2;                      // 剩余指数拆半
+            factor = (long long)factor * factor % p;  // 因子自乘翻倍
+        }
+    }
+    return result % p;
+}
+
+int main(void) {
+    int a, b, p;
+    scanf("%d%d%d", &a, &b, &p);
+    int c = fastpowermod(a, b, p);
+    printf("%d^%d mod %d=%d", a, b, p, c);
+    return 0;
+}
