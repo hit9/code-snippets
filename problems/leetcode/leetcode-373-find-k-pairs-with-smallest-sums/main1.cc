d055cc99a7acdc20d1f5d3cbe607fb45af7f43be
hit9 <hit9@icloud.com>
2023-11-27 10:54:55 +0800

    update

--- problems/leetcode/leetcode-373-find-k-pairs-with-smallest-sums/main1.cc
+++ problems/leetcode/leetcode-373-find-k-pairs-with-smallest-sums/main1.cc
@@ -13,18 +13,18 @@ class Solution {
    public:
     // 找到两个数组中数对和不超过给定值的数对的数量
     // 等效于下面的 findPairs，但是只是计数
-    ull count(const vector<int>& nums1, const vector<int>& nums2, ll val) {
+    ull count(const vector<int>& a, const vector<int>& b, ll val) {
         ull ans = 0;
-        // 对 nums1 中的每一项 x 来说，相当于找 nums2 中 <= val-x 的上界的问题
-        for (auto x : nums1) {
+        // 对 a 中的每一项 x 来说，相当于找 b 中 <= val-x 的右界的问题
+        for (auto x : a) {
             int target = val - x;
-            // 注意防止全部太大的情况, 此时找不到上界
-            if (!(nums2[0] > target)) {
+            // 注意防止全部太大的情况, 此时找不到右界
+            if (!(b[0] > target)) {
                 int l = 0;
-                int r = nums2.size() - 1;
+                int r = b.size() - 1;
                 while (l < r) {
                     int m = (l + r + 1) >> 1;
-                    if (nums2[m] <= target)
+                    if (b[m] <= target)
                         l = m;
                     else
                         r = m - 1;
@@ -38,31 +38,31 @@ class Solution {
     // 找到两个数组中数对和不超过给定值的数对
     // 并按照从小到大的顺序输出
     // 最多 k 个
-    void findPairs(const vector<int>& nums1, const vector<int>& nums2, ll val,
-                   int k, vector<vector<int>>& ans) {
+    void findPairs(const vector<int>& a, const vector<int>& b, ll val, int k,
+                   vector<vector<int>>& ans) {
         // 大顶堆
         auto cmp = [&](const P& a, const P& b) {
             return a.first + a.second < b.first + b.second;
         };
         priority_queue<P, vector<P>, decltype(cmp)> q(cmp);
 
-        // 对 nums1 中的每一项 x 来说，相当于找 nums2 中 <= val-x 的上界的问题
-        for (auto x : nums1) {
+        // 对 a 中的每一项 x 来说，相当于找 b 中 <= val-x 的右界的问题
+        for (auto x : a) {
             int target = val - x;
-            // 注意防止全部太大的情况, 此时找不到上界
-            if (!(nums2[0] > target)) {
+            // 注意防止全部太大的情况, 此时找不到右界
+            if (!(b[0] > target)) {
                 int l = 0;
-                int r = nums2.size() - 1;
+                int r = b.size() - 1;
                 while (l < r) {
                     int m = (l + r + 1) >> 1;
-                    if (nums2[m] <= target)
+                    if (b[m] <= target)
                         l = m;
                     else
                         r = m - 1;
                 }
                 // 收集起来
                 for (int j = 0; j <= l; j++) {
-                    q.push({x, nums2[j]});
+                    q.push({x, b[j]});
                     // 如果数量超过 k 了，置换出最大的
                     if (q.size() >= k + 1) q.pop();
                 }
@@ -78,34 +78,33 @@ class Solution {
         reverse(ans.begin(), ans.end());
     }
 
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
-                                       int k) {
+    vector<vector<int>> kSmallestPairs(vector<int>& a, vector<int>& b, int k) {
         vector<vector<int>> ans;
 
         // 先二分判定找到符合条件的第 k 小的和
-        ll l = nums1[0] + nums2[0];
-        ll r = nums1[nums1.size() - 1] + nums2[nums2.size() - 1];
+        ll l = a[0] + b[0];
+        ll r = a[a.size() - 1] + b[b.size() - 1];
 
         // 相当于找满足 count(mid) >= k 的下界
         while (l < r) {
             int m = (l + r) >> 1;
-            if (count(nums1, nums2, m) >= k)
+            if (count(a, b, m) >= k)
                 r = m;
             else
                 l = m + 1;
         }
 
         // 然后收集这些数对
-        findPairs(nums1, nums2, l, k, ans);
+        findPairs(a, b, l, k, ans);
         return ans;
     }
 };
 
 int main(void) {
     Solution s;
-    vector<int> nums1{1, 2};
-    vector<int> nums2{3};
-    for (const auto& item : s.kSmallestPairs(nums1, nums2, 3))
+    vector<int> a{1, 2};
+    vector<int> b{3};
+    for (const auto& item : s.kSmallestPairs(a, b, 3))
         cout << item[0] << "," << item[1] << endl;
     return 0;
 }
