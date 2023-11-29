dee0d4625f120768705f97ceb88232b924f7e5dd
hit9 <hit9@icloud.com>
2023-11-29 19:00:27 +0800

    暂时想不到更好的剪枝了..

--- problems/luogu/P1410/main1.cc
+++ problems/luogu/P1410/main1.cc
@@ -1,4 +1,6 @@
 // https://www.luogu.com.cn/problem/P1410
+//
+// 目前这个 DFS 会超时..
 
 #include <iostream>
 #include <vector>
@@ -11,18 +13,40 @@ using namespace std;
 // b 是数组 a 的最后一个数字
 // i 是当前 nums 中的迭代下标
 // k 是 nums.size()/2
-bool dfs(vector<int>& nums, int n, int k, int na, int a, int nb, int b, int i) {
+bool dfs(vector<int>& nums, int n, int k, int na, int a, int nb, int b, int i,
+         vector<int>& p, vector<int>& q) {
     if (na == k && nb == k) return true;
     if (i >= n) return false;
 
+    // 剪枝：如果右侧的最大值比当前两个值都小，就提前退出
+    if (nums[q[i]] <= a && nums[q[i]] <= b) return false;
+
+    // 如果右侧的最大值的位置太靠左，就提前退出
+    if (q[i] < k - 1) return false;
+
+    // 剪枝：如果右侧存在一个最小值比两个当前值都小，就提前退出
+    if (nums[p[i]] <= a && nums[p[i]] <= b) return false;
+
+    // 如果右侧最小值的位置太靠右, 就提前退出
+    if (nums[p[i]] > a && nums[p[i]] <= b && k - na > (n - p[i])) {
+        return false;
+    }
+    if (nums[p[i]] > b && nums[p[i]] <= a && k - nb > (n - p[i])) {
+        return false;
+    }
+    if (nums[p[i]] > a && nums[p[i]] > b &&
+        (n - p[i]) < std::min(k - na, k - nb)) {
+        return false;
+    }
+
     // 平衡填数，如果 na 已经行进太多，则先填 b
     // 这样 a 和 b 两个子数组的大小不至于相差太多
-    if (na > nb + 1) return dfs(nums, n, k, nb, b, na, a, i);
+    if (na > nb + 1) return dfs(nums, n, k, nb, b, na, a, i, p, q);
 
     // 考虑 nums[i] 放 a 数组
     int x = nums[i];
     if (na < k && x > a) {
-        if (dfs(nums, n, k, na + 1, x, nb, b, i + 1)) return true;
+        if (dfs(nums, n, k, na + 1, x, nb, b, i + 1, p, q)) return true;
     }
 
     // 剪枝: 另一个已经满了, 但是上面考察放入 a 仍不行，
@@ -31,19 +55,41 @@ bool dfs(vector<int>& nums, int n, int k, int na, int a, int nb, int b, int i) {
 
     // 考虑 nums[i] 放 b 数组
     if (nb < k && x > b) {
-        if (dfs(nums, n, k, na, a, nb + 1, x, i + 1)) return true;
+        if (dfs(nums, n, k, na, a, nb + 1, x, i + 1, p, q)) return true;
     }
     if (na == k) return false;
     return false;
 }
 
-bool check(vector<int>& nums, int n) {
+bool check(vector<int>& nums, vector<int>& p, vector<int>& q, int n) {
+    // 提前预处理出数组 p
+    // p[i] 记录右侧区间上 [i..] 的最小值的位置
+    p[n - 1] = n - 1;
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[p[i + 1]])
+            p[i] = i;
+        else
+            p[i] = p[i + 1];
+    }
+
+    // q[i] 记录右侧区间上 [i..] 的最大值的位置
+    q[n - 1] = n - 1;
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] > nums[q[i + 1]])
+            q[i] = i;
+        else
+            q[i] = q[i + 1];
+    }
+
     // 可以直接先假定第一个元素放入 a
-    return dfs(nums, n, n / 2, 1, nums[0], 0, -1, 1);
+    return dfs(nums, n, n / 2, 1, nums[0], 0, -1, 1, p, q);
 }
 
 int main(void) {
     vector<int> nums(2001);
+    vector<int> p(2001);  // 辅助数组: 记录右侧最小值的位置
+    vector<int> q(2001);  // 辅助数组: 记录右侧最大值的位置
+
     // 多组测试数据
     while (1) {
         // 输入
@@ -55,7 +101,7 @@ int main(void) {
         for (int j = 0; j < n; j++) cin >> nums[j];
 
         // 调用
-        if (check(nums, n))
+        if (check(nums, p, q, n))
             cout << "Yes!" << endl;
         else
             cout << "No!" << endl;
