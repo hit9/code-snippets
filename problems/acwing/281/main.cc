4871cf7e2978cc960f3b419b1290caea505e0f8a
hit9 <hit9@icloud.com>
2023-11-30 18:36:25 +0800

    Add acwing 281

---
+++ problems/acwing/281/main.cc
@@ -0,0 +1,28 @@
+
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+using ull = unsigned long long;
+int kp(int n) {
+    ull f[n + 1];
+    memset(f, 0, sizeof f);
+
+    f[0] = 1;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j <= n; j++) {
+            f[j] += f[j - i] % 2147483648;
+        }
+    }
+    return f[n] % 2147483648;
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    getchar();
+    cout << kp(n) << endl;
+    return 0;
+}
