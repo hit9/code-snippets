571212019506efe61c2605d63be650e0cea14458
hit9 <hit9@icloud.com>
2023-12-02 17:17:39 +0800

    minor update

--- problems/luogu/CF10D/main.cc
+++ problems/luogu/CF10D/main.cc
@@ -16,43 +16,32 @@ int m;  // b 的大小
 // dp[i][j] 表示 a[1..i] 和 b[1..j] 上的以 b[j] 结尾的 LCIS 长度
 int dp[N][N] = {0};
 
-// path[i][j] = k 表示 dp[i][j] 的值来自于 (i-1, k)
+// path[i][j] = k 表示 dp[i][j] 的值来自于 dp[i-1][k]
 int path[N][N];
 
 void slove() {
-    int ans = 0;               // 最长长度
-    int ans_i = 0, ans_j = 0;  // 最长长度的来源位置
+    // 最长 LCIS 长度, 取得答案的方格位置的 i, j 坐标
+    int ans = 0, ans_i = 0, ans_j = 0;
 
     for (int i = 1; i <= n; i++) {
         // mx 跟进 dp[i-1][k] (k 属于 0..j) 上的满足 b[k] < a[i]=b[j] 的 dp
         // 最大值
-        int mx = 0;
-        int mj = 0;
+        int mx = 0, mj = 0;
 
         for (int j = 1; j <= m; j++) {
             if (a[i] != b[j]) {
                 dp[i][j] = dp[i - 1][j];
-                // 拷贝来源
                 path[i][j] = path[i - 1][j];
             } else {  // a[i] == b[j]
                 dp[i][j] = mx + 1;
-                // 真正来源
+                // 记录由哪个 j 扩充来源
                 path[i][j] = mj;
             }
 
             // 维护 mx 和 mj
-            if (b[j] < a[i]) {
-                if (mx < dp[i - 1][j]) {
-                    mx = dp[i - 1][j];
-                    mj = j;
-                }
-            }
-
-            if (dp[i][j] > ans) {
-                ans = dp[i][j];
-                ans_i = i;
-                ans_j = j;
-            }
+            if (b[j] < a[i] && mx < dp[i - 1][j]) mx = dp[i - 1][j], mj = j;
+            // 记录答案
+            if (ans < dp[i][j]) ans = dp[i][j], ans_i = i, ans_j = j;
         }
     }
 
@@ -60,8 +49,8 @@ void slove() {
     cout << ans << endl;
 
     // 回溯一个最长序列
-    int q = ans;
-    int i = ans_i, j = ans_j;
+    int q = ans, i = ans_i, j = ans_j;
+
     while (q > 0) {
         // 只有 j > 0 才是真正的来源
         if (j > 0) c[q--] = b[j];
