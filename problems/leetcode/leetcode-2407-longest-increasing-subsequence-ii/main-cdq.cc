1006911551b5864586363dabdd6fe7d3bf4d4be7
hit9 <hit9@icloud.com>
2023-12-10 17:00:03 +0800

    Renames

--- problems/leetcode/leetcode-2407-longest-increasing-subsequence-ii/main-cdq.cc
+++ problems/leetcode/leetcode-2407-longest-increasing-subsequence-ii/main-cdq.cc
@@ -23,31 +23,31 @@ class Solution {
         sort(a.begin() + start, a.begin() + mid + 1, cmp_value);
         sort(a.begin() + mid + 1, a.begin() + end + 1, cmp_value);
 
-        // 对右边的每个 j, 找到左边值比它小的 i 的最大的 dp 值
-        // 因为已经按值有序，所以 i 不必回溯
+        // 对右边的每个 i, 找到左边值比它小的 j 的最大的 dp 值
+        // 因为已经按值有序，所以 j 不必回溯
         // 采用单调队列来维护区间 dp 最值
         // 此部分的总均摊时间复杂度是 n
 
-        int j = mid + 1;
-        int i = start;
+        int i = mid + 1;
+        int j = start;
         deque<int> q;  // 维护最大值的单调递减队列, 存储下标
-        while (j <= end) {
-            while (i <= mid && a[i].first < a[j].first) {
+        while (i <= end) {
+            while (j <= mid && a[j].first < a[i].first) {
                 // 维护右端, 弹出比 a[i] 的 dp 值小的
-                while (!q.empty() && dp[a[q.back()].second] < dp[a[i].second])
+                while (!q.empty() && dp[a[q.back()].second] < dp[a[j].second])
                     q.pop_back();
-                // 加入 a[i]
-                q.push_back(i++);
+                // 加入 a[j]
+                q.push_back(j++);
 
                 // 维护左端: 弹出左端已经脱离 k 限制的
-                while (!q.empty() && a[q.front()].first + k < a[j].first)
+                while (!q.empty() && a[q.front()].first + k < a[i].first)
                     q.pop_front();
             }
-            while (!q.empty() && a[q.front()].first + k < a[j].first)
+            while (!q.empty() && a[q.front()].first + k < a[i].first)
                 q.pop_front();
             int ma = q.empty() ? 0 : dp[a[q.front()].second];
-            dp[a[j].second] = std::max(dp[a[j].second], ma + 1);
-            j++;
+            dp[a[i].second] = std::max(dp[a[i].second], ma + 1);
+            i++;
         }
 
         // 恢复秩序, 其实左边没有必要恢复
