50c97443a166b2863af415a76ce3ea34150f4e6b
hit9 <hit9@icloud.com>
2023-12-14 16:53:33 +0800

    Add P4597

---
+++ problems/luogu/P4597/main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+using ull = unsigned long long;
+
+ull slove(const vector<int> &a) {
+    priority_queue<int> q;
+    ull ans = 0;
+    for (auto x : a) {
+        q.push(x);
+        q.push(x);
+        ans += q.top() - x;
+        q.pop();
+    }
+    return ans;
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    getchar();
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    cout << slove(a) << endl;
+    return 0;
+}
