dd1c61fde02ef21d37ec0891fe2fe49c0741379f
hit9 <hit9@icloud.com>
2023-12-21 19:13:39 +0800

    Add leetcode 132

---
+++ problems/leetcode/leetcode-132-palindrome-partitioning-ii/main.cc
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <deque>
+#include <string>
+using namespace std;
+class Solution {
+   public:
+    int minCut(string s) {
+        int n = s.size();
+        int f[n + 1];
+        f[0] = -1;
+        deque<int> q;
+
+        for (int i = 1; i <= n; i++) {
+            f[i] = f[i - 1] + 1;
+            int k = q.size();
+            while (k--) {
+                int j = q.back();
+                q.pop_back();
+                if (j && s[j - 1] == s[i - 1]) {
+                    f[i] = min(f[i], f[j - 1] + 1);
+                    q.push_front(j - 1);
+                }
+            }
+            q.push_front(i - 1);
+            q.push_front(i);
+        }
+
+        return f[n];
+    }
+};
+
