40d706f12239c317c6481dd721dcc967f04338af
hit9 <hit9@icloud.com>
2024-01-31 23:16:09 +0800

    添加多重背包

--- dp/knapsack/README.md
+++ dp/knapsack/README.md
@@ -2,3 +2,4 @@ https://writings.sh/post/knapsack
 
 - [01 背包](main1.cc)
 - [完全背包](main2.cc)
+- [多重背包](main3.cc)
---
+++ dp/knapsack/main3.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+// 多重背包
+
+const int N = 10005;
+
+int v[N], w[N], c[N];  // 物品体积、价值、每种物品的个数
+int n, m;              // 物品种数、背包体积
+
+int f[N];  // 背包放入总体积为 j 的物品的最大价值 f[j]
+
+int solve() {
+    memset(f, 0xcf, sizeof f);
+    f[0] = 0;
+    for (int i = 1; i <= n; i++) {           // 物品种数
+        for (int k = 1; k <= c[i]; k++)      // 每种物品的个数
+            for (int j = m; j >= v[i]; j--)  // 体积
+                f[j] = max(f[j], f[j - v[i]] + w[i]);
+    }
+    int ans = 0;
+    for (int j = 0; j <= m; j++) ans = max(ans, f[j]);
+    return ans;
+}
