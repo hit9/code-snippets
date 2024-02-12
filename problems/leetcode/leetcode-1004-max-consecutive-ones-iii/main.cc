4b2aed769b8a48f275a1a872d7a62c728978f644
hit9 <hit9@icloud.com>
2024-02-12 16:31:12 +0800

    minor improvement

--- problems/leetcode/leetcode-1004-max-consecutive-ones-iii/main.cc
+++ problems/leetcode/leetcode-1004-max-consecutive-ones-iii/main.cc
@@ -1,7 +1,6 @@
 // https://leetcode.cn/problems/max-consecutive-ones-iii
 // 1004. 最大连续1的个数 III
 
-
 #include <vector>
 using namespace std;
 class Solution {
@@ -9,15 +8,16 @@ class Solution {
     int longestOnes(vector<int>& nums, int k) {
         int k0 = 0;  // 当前 0 的个数
         int ans = 0;
-        for (int left = 0, right = 0; right < nums.size(); right++) {
-            if (nums[right] == 0) k0++;
+        for (int L = 0, R = 0; R < nums.size(); R++) {
+            if (nums[R] == 0) k0++;
 
-            while (k0 > k && left <= right) {
-                if (nums[left] == 0) k0--;
-                left++;
+            // L <= R 而不是 L < R，允许空窗口出现，因为 k 可能是 0
+            // 空窗口的时候，答案恰好是 0
+            while (L <= R && k0 > k) {
+                if (nums[L++] == 0) k0--;
             }
 
-            ans = std::max(ans, right - left + 1);
+            ans = std::max(ans, R - L + 1);
         }
         return ans;
     }
