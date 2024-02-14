c6b5cf64b3750deb2acd0d9e8b8222c5852a4c7e
hit9 <hit9@icloud.com>
2024-02-14 15:50:03 +0800

    minor improve

--- problems/leetcode/leetcode-438-find-all-anagrams-in-a-string/main1.cc
+++ problems/leetcode/leetcode-438-find-all-anagrams-in-a-string/main1.cc
@@ -10,9 +10,9 @@ class Solution {
         unordered_map<char, int> d;  // p 中的各个字符的出现次数
         int a = 0;                   // p 中不同的字符个数
         for (auto ch : p) if (++d[ch] == 1) a++;
-        int L = 0, b = 0;  // b 是窗口内的不同的字符的个数
+        int  b = 0;  // b 是窗口内的扣减了 p 中字符的种类数
         vector<int> ans;
-        for (int R = 0; R < s.size(); R++) {
+        for (int L =0, R = 0; R < s.size(); R++) {
             // 扣减出现频次, 并且, 如果扣减后是 0 , 则记录一次 b
             if (--d[s[R]] == 0) b++;
             // 恢复出现频次, 如果恢复前是 0, 则也要恢复 b
