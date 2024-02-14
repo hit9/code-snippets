7dc4be2632a5425e2824a420b88047a055c4e565
hit9 <hit9@icloud.com>
2024-02-14 21:32:39 +0800

    优化

--- problems/leetcode/leetcode-76-minimum-window-substring/main.cc
+++ problems/leetcode/leetcode-76-minimum-window-substring/main.cc
@@ -6,25 +6,19 @@ class Solution {
    public:
     string minWindow(string s, string t) {
         unordered_map<char, int> d1;  // t 内的字符频次计数
-        unordered_map<char, int> d2;  // 窗口内的字符频次计数
         int c = 0;  // 还需要统计的字符种类, 初始化为 t 中的字符种类
         for (auto ch : t) {
             d1[ch]++;
             if (d1[ch] == 1) c++;
         }
-        int L = 0;
+        unordered_map<char, int> d2;  // 窗口内的字符频次计数
         int ans_L = 0, ans_size = 0x3f3f3f3f;
-        for (int R = 0; R < s.size(); R++) {
+        for (int L = 0, R = 0; R < s.size(); R++) {
             // 维护右端: 窗口内计数+1
             // 并且如果是需要的字符, 则维护需要的种类 c
             if (++d2[s[R]] == d1[s[R]]) c--;
             // 维护左端: 左端 L 字符的出现次数超过 t 中的
-            while (L < R && d2[s[L]] > d1[s[L]]) {
-                d2[s[L]]--;
-                // 恢复需要的字符种类的统计
-                if (d2[s[L]] == 0 && d1[s[L]]) c++;
-                L++;
-            }
+            while (L < R && d2[s[L]] > d1[s[L]]) d2[s[L++]]--;
             // 维护答案
             if (c == 0 && ans_size > (R - L + 1)) {
                 ans_L = L;
