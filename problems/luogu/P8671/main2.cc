353c4d12e481f1f5ed39a2b895b3448a19946fd9
hit9 <hit9@icloud.com>
2024-02-16 16:49:20 +0800

    添加方法三

---
+++ problems/luogu/P8671/main2.cc
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+using ll = unsigned long long;
+
+int josephus(int n, int k, ll q) {
+    if (k == 1) return n - 1;
+    for (q = q * k + k - 1; q >= n; q = q - n + (q - n) / (k - 1))
+        ;
+    return q;
+}
+
+int main(void) {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    printf("%d\n", josephus(n, k, n - 1) + 1);
+    return 0;
+}
