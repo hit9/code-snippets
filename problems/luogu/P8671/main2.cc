2d54b9105a338923626fc79add5f2dd1024efd76
hit9 <hit9@icloud.com>
2024-02-16 17:57:19 +0800

    一点注释

--- problems/luogu/P8671/main2.cc
+++ problems/luogu/P8671/main2.cc
@@ -3,6 +3,7 @@ using namespace std;
 
 using ll = unsigned long long;
 
+// q will always < nk
 int josephus(int n, int k, ll q) {
     if (k == 1) return n - 1;
     for (q = q * k + k - 1; q >= n; q = q - n + (q - n) / (k - 1))
