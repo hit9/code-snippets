d75ff7c369014d61f1921a5d3c8cecee245a7f80
hit9 <hit9@icloud.com>
2024-02-16 16:43:30 +0800

    minor improve

--- problems/luogu/P8671/main1.cc
+++ problems/luogu/P8671/main1.cc
@@ -1,13 +1,12 @@
 #include <cstdio>
 using namespace std;
+
 int josephus(int n, int k) {
     if (n == 1) return 0;
     if (k == 1) return n - 1;
     if (k > n) return (josephus(n - 1, k) + k) % n;
-    int f = josephus(n - n / k, k);
-    f -= n % k;
-    f += f < 0 ? n : (f / (k - 1));
-    return f;
+    int f = josephus(n - n / k, k) - n % k;
+    return f + (f < 0 ? n : (f / (k - 1)));
 }
 
 int main(void) {
