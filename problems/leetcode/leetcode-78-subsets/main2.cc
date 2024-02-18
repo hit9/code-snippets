adaabcea35f7aaa5cdded9d71f586d9e3f411f82
hit9 <hit9@icloud.com>
2024-02-18 17:36:03 +0800

    Add subsets ii

--- problems/leetcode/leetcode-78-subsets/main2.cc
+++ problems/leetcode/leetcode-78-subsets/main2.cc
@@ -3,20 +3,20 @@
 using namespace std;
 class Solution {
    public:
-    void dfs(vector<vector<int>>& ans, vector<int>& path, vector<int>& nums,
+    void dfs(vector<vector<int>>& ans, vector<int>& path, vector<int>& a,
              int start) {
         ans.push_back(path);
-        for (int i = start; i < nums.size(); i++) {
-            path.push_back(nums[i]);  // 选择当前元素，进一步更深的递归
-            dfs(ans, path, nums, i + 1);
+        for (int i = start; i < a.size(); i++) {
+            path.push_back(a[i]);  // 选择当前元素，进一步更深的递归
+            dfs(ans, path, a, i + 1);
             path.pop_back();  // 撤销选择，从下一个起点开始选择
         }
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsets(vector<int>& a) {
         vector<vector<int>> ans;
         vector<int> path;
-        dfs(ans, path, nums, 0);
+        dfs(ans, path, a, 0);
         return ans;
     }
 };
