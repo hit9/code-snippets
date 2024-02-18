116c1ac6d98dfec476a35a80ff28aadadd605bed
hit9 <hit9@icloud.com>
2024-02-18 22:23:17 +0800

    再加一个方法

---
+++ problems/leetcode/leetcode-78-subsets/main3.cc
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+class Solution {
+   public:
+    vector<int> chosen;
+    vector<vector<int>> ans;
+    void dfs(vector<int>& a, int k) {
+        if (k == a.size()) {
+            ans.push_back(chosen);
+            return;
+        }
+
+        dfs(a, k + 1);  // 不选 a[k], 继续问题 dfs(k+1..N)
+
+        chosen.push_back(a[k]);  // 选 a[k]
+        dfs(a, k + 1); // 继续问题 dfs(k+1..N)
+        chosen.pop_back();  // 撤销
+    }
+
+    vector<vector<int>> subsets(vector<int>& a) {
+        dfs(a, 0);
+        return ans;
+    }
+};
