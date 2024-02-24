93a822a13531c9e1d77966b8c85fb6df102cc8e6
hit9 <hit9@icloud.com>
2024-02-24 19:12:44 +0800

    Add leetcode-279-perfect-squares

---
+++ problems/leetcode/leetcode-279-perfect-squares/main.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+const int K = 100 + 5;
+const int M = 10000 + 5;
+int f[M];
+int v[K];  // 体积数组
+
+class Solution {
+   public:
+    int numSquares(int m) {
+        // 预处理体积数组, 最多到 sqrt(m)
+        int k = 1;
+        while (1) {
+            int s = k * k;
+            if (s > m) break;
+            v[k++] = s;
+        }
+
+        // 代入完全背包
+        memset(f, 0x3f, sizeof f);
+        f[0] = 0;
+
+        for (int i = 1; i <= k; i++) {
+            for (int j = v[i]; j <= m; j++) {
+                f[j] = min(f[j], f[j - v[i]] + 1);
+            }
+        }
+        return f[m];
+    }
+};
