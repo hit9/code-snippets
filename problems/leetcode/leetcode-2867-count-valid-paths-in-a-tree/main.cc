4dfbaac64921fef5e3d6a66aa0a2be27ce8ca700
hit9 <hit9@icloud.com>
2024-02-27 18:22:26 +0800

    improve comments

--- problems/leetcode/leetcode-2867-count-valid-paths-in-a-tree/main.cc
+++ problems/leetcode/leetcode-2867-count-valid-paths-in-a-tree/main.cc
@@ -14,65 +14,57 @@ class Solution {
             if (!pt[i]) continue;
             for (int j = i * i; j <= n; j += i) pt[j] = false;
         }
-        // 预处理边
+        // 预处理边 (存图)
         vector<vector<int>> ed(n + 1);
         for (auto& e : edges) {
             ed[e[0]].push_back(e[1]);
             ed[e[1]].push_back(e[0]);
         }
         // f[i] 计算子树 i 以根为端点的只含合数的路径 (1个节点也行)
-        // g[i] 计算以素数 i 为端点的合法路径数量 (至少2个节点)
-        vector<int> f(n + 1, 0), g(n + 1, 0);
+        vector<int> f(n + 1, 0);
         for (int i = 1; i <= n; i++)
             if (!pt[i]) f[i] = 1;
 
-        // 以 1 为根, 先计算在素数节点子树内的路径 (后序计算 pushup)
+        // 后序计算 pushup
         vector<bool> visit(n + 1, false);
         function<void(int)> up = [&](int i) {
             visit[i] = true;
             for (auto j : ed[i]) {
                 if (visit[j]) continue;
                 up(j);
-                if (pt[i]) {       // i 是素数
-                    g[i] += f[j];  // 来自 j 的以 i 为端点的合法路径
-                } else {
-                    f[i] += f[j];  // 以 i 为端点的合数路径
-                }
+                // 以 i 为端点的合数路径
+                if (!pt[i]) f[i] += f[j];
             }
         };
         up(1);
 
-        // 前序计算, 不在以素数i为根的子树内的合法路径
-        fill(visit.begin(), visit.end(), false);
+        // 前序计算
+        fill(visit.begin(), visit.end(), false); // 复用下 visit 数组
         function<void(int)> down = [&](int i) {
             visit[i] = true;
             for (auto j : ed[i]) {
                 if (visit[j]) continue;
-                if (pt[j]) {
-                    g[j] += f[i];  // 来自 i 以 j 为下端点的路径数
-                } else {
-                    // 来自 i 的以 j 为端点的合数路径
-                    // 注意减去自己的贡献
-                    if (!pt[i]) f[j] += f[i] - f[j];
-                }
+                // 来自 i 的以 j 为端点的合数路径, 注意减去自己原本的贡献
+                if (!pt[j] && !pt[i]) f[j] += f[i] - f[j];
                 down(j);
             }
         };
 
         down(1);
 
+        // 汇总答案
         ll ans = 0;
         for (int i = 1; i <= n; i++) {
             if (!pt[i]) continue;
-            ans += g[i];  // 以 i 为端点的路径
-            // 另需考虑路过 i 的路径数
+            // s 是 f 的和, 即以 i 为端点的合法路径
+            int s = 0;
+            // q 计算 f1 * f2 的和, 即路过 i 的合法路径
+            ll q = 0;
             for (auto j : ed[i]) {
-                if (!f[j]) continue;
-                for (auto k : ed[i]) {
-                    if (k == j) break;
-                    ans += (ll)f[k] * f[j];
-                }
+                q += s * f[j];
+                s += f[j];
             }
+            ans += s + q;
         }
         return ans;
     }
