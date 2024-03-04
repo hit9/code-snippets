bf6ba1a628cf550b32432b707c769391d3c259fb
hit9 <hit9@icloud.com>
2024-03-04 21:20:49 +0800

    reformat

--- problems/leetcode/leetcode-207-course-schedule/main.cc
+++ problems/leetcode/leetcode-207-course-schedule/main.cc
@@ -2,16 +2,16 @@
 using namespace std;
 class Solution {
    public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> edges(numCourses);
-        vector<int> deg(numCourses, 0);
-        for (auto p : prerequisites) {
-            edges[p[0]].push_back(p[1]);
-            deg[p[1]]++;  // p[1] 的入度+1
+    bool canFinish(int n, vector<vector<int>>& p) {
+        vector<vector<int>> edges(n);
+        vector<int> deg(n, 0);
+        for (auto e : p) {
+            edges[e[0]].push_back(e[1]);
+            deg[e[1]]++;  // p[1] 的入度+1
         }
 
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < n; i++) {
             if (deg[i] == 0) q.push(i);  // 入度为 0 的作为 bfs 起点
         }
 
@@ -25,6 +25,6 @@ class Solution {
                 if (--deg[y] == 0) q.push(y);
             }
         }
-        return cnt == numCourses;
+        return cnt == n;
     }
 };
