19942fda279148a3e76ae1855d14482b13aa17e9
hit9 <hit9@icloud.com>
2024-03-16 22:36:23 +0800

    Add P1272

---
+++ problems/luogu/P1272/main.cc
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+// P1272
+// 一个以1号节点为根的有向树,最少断几个边可以分离出一颗节点数目恰好为 P
+// 的子树出来.
+
+using namespace std;
+
+const int N = 151;
+
+int n, p;
+
+// 边
+vector<int> edges[N];
+
+// 子树 x 的大小
+int sz[N];
+
+// f[x][j] 表示子树 x 分离出来大小为 j 的子树, 且包含 x 的
+// 最少需要删除的边的数目
+int f[N][N];
+
+// dfs1 预处理 size
+void dfs1(int x) {
+    sz[x] = 1;
+    for (auto y : edges[x]) {
+        dfs1(y);
+        sz[x] += sz[y];
+    }
+}
+
+// dfs2 处理 f
+void dfs2(int x) {
+    f[x][1] = edges[x].size();  // 只保留 x 的时候
+    for (auto y : edges[x]) {
+        dfs2(y);
+        // 枚举要保留的节点大小 j, 必须保留 x, 所以 j 至少1
+        for (int j = p; j >= 1; j--) {
+            // 枚举子树 y 中分配了多少节点 k, 至多 j - 1 (保留 x)
+            for (int k = 0; k <= j - 1; k++) {
+                // 因为必须保留根x, 所以边 (x,y) 不能删
+                // 那么就应该采用 f[x][j-k] - 1
+                f[x][j] = min(f[x][j], f[y][k] + f[x][j - k] - 1);
+            }
+        }
+    }
+}
+
+int solve() {
+    memset(f, 0x3f, sizeof f);
+    dfs1(1);
+    dfs2(1);
+
+    // 答案取整体最小
+    // 注意,根节点的时候,即 f[1][p]
+    int ans = f[1][p];
+    // 但是其他节点,要考虑把它和上游的连接也切掉,所以+1
+    for (int x = 2; x <= n; x++) ans = min(ans, f[x][p]+1);
+    return ans;
+}
+
+int main(void) {
+    scanf("%d%d", &n, &p);
+    int k = n - 1;
+    while (k--) {
+        int fa, x;
+        scanf("%d%d", &fa, &x);
+        edges[fa].push_back(x);
+    }
+    printf("%d\n", solve());
+    return 0;
+}
